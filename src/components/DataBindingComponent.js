b00967bbeef091e0d2ede0451d42ddd08b362678
Shivangi Sharma <sshivangi882@gmail.com>
2024-07-10 08:53:33 +0530

    Data Binding in progress

--- src/components/DataBindingComponent.js
+++ src/components/DataBindingComponent.js
@@ -1,19 +1,24 @@
 
  
 export default function DataBindingComponent(){
-    var size ={
-        Width: 450 ,
-        Height :100
-    }
-    return (
-       <div className="container">
-            <h2>Property Binding</h2>
-            <table border="1" width={size.width} Height = {size.Height}>
-                <tr>
-                    <td>Welcome to React</td>
-                </tr>
-            </table>
+    var menu =[
+        {Category : "Electronics", Products:["Samsung TV", "Mobile"]},
+        {Category : "Footwear", Products:["Nike Causals ", "Lee Boot"]}
+
+    ]
+    return(
+        <div className="container">
+            <h2>Categories</h2>
+            <ol>
+                {
+                    menu.map(item =>
+                        <li>{item.Category}</li>
+                    )
+                }
+            </ol>
+
 
         </div>
     )
+                    
 }
