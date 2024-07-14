02ae7c2a0b438535b5addf7ba3bc3881da8b4779
Shivangi Sharma <sshivangi882@gmail.com>
2024-07-14 09:42:21 +0530

    shopping component added

--- src/components/ShoppingComponent.js
+++ src/components/ShoppingComponent.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 export default function ShoppingComponent(){
 const [categories, setCategories] = useState([]);
+const [products, setProdutcs]= useState([]);
 
 function LoadCategories(){
     fetch("https://fakestoreapi.com/products/categories")
@@ -13,9 +14,19 @@ function LoadCategories(){
     })
 }
 
+function LoadProducts(){
+    fetch("https://fakestoreapi.com/products")
+    .then(response=>response.json())
+    .then(data=>{
+        setProdutcs(data);
+    })
+}
+
 useEffect(()=>{
     LoadCategories();
+    LoadProducts();
 },[])
+
 
     return(
         <div className="container-fluid">
@@ -31,7 +42,7 @@ useEffect(()=>{
                             <select className="form-select">
                                 {
                                     categories.map(category=>
-                                        <option key={category}>{category.toUpperCase}</option>
+                                        <option key={category}>{category}</option>
                                     )
                                 }
                             </select>
@@ -39,8 +50,30 @@ useEffect(()=>{
                     </div>
 
                 </nav>
-                <main>
+                <main className="col-9 f-flex flex-wrap overflow-auto style={{height:'600px'}}">
+                        {
+                            products.map(product =>
+                                <div className="card m-2 p-2 w-25 ">
+                            <img src={product.image} className="card-img-top" height="150" alt="img" />
+                            <div className="card-header">
+                                <p>{product.title}</p>
+                            </div>
+                            <div className="card-body">
+                                <dl>
+                                    <dt>Price</dt>
+                                    <dd>{product.price}</dd>
+                                    <dt>Rating</dt>
+                                    <dd>
+                                        <span className="bi bi-star-fill text-success"></span>
+                                        {product.rating.rate} <span>[{product.rating.count}]</span>
+                                    </dd>
 
+                                        </dl>
+                                    </div>
+                    
+                                </div>
+                            )
+                        }
                 </main>
             </section>
         </div>
