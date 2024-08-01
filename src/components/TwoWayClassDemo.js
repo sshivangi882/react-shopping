685b28346f77ad7e9ddbae711cdde2592ebd3437
Shivangi Sharma <sshivangi882@gmail.com>
2024-08-01 09:53:18 +0530

    TwoWayClass

---
+++ src/components/TwoWayClassDemo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class TwoWayClassDemo extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {    // derived class state
+
+        }
+    }
+    render(){
+        return(
+            <div className="container-fluid">
+                <h2>
+                    Product details
+                </h2>
+            </div>
+        )
+    }
+}
