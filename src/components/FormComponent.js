41758d2cb4ed330b25fb046f4ef240c8262354e7
Shivangi Sharma <sshivangi882@gmail.com>
2024-08-05 09:21:26 +0530

    validations

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,40 +1,62 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function FormComponent() {
-  const [users] = useState([
-    { UserId: "john" },
-    { UserId: "john12" },
-    { UserId: "david" },
-    { UserId: "john_nit" },
-  ]);
-
+function FormComponent() {
   const [useMsg, setUseMsg] = useState("");
-  const [isUserValid, setUserValid] = useState(false);
+  const [isUserValid, setIsUserValid] = useState(false);
+  const [pwdMsg, setPwdMsg] = useState("");
+
+  function HideUserMsg() {
+    setUseMsg("");
+  }
 
   function VerifyUserId(e) {
-    for (var user of users) {
-      if (user.UserId == e.target.value) {
-        setUseMsg("User Id already taken! Try another");
-        setUserValid(false);
-        break;
-      } else {
-        setUseMsg("User Id available");
-        setUserValid(true);
-      }
+    const userId = e.target.value;
+    if (userId.length >= 6) {
+      setUseMsg("User ID is valid");
+      setIsUserValid(true);
+    } else {
+      setUseMsg("User ID must be at least 6 characters long");
+      setIsUserValid(false);
     }
   }
+
+  function VerifyPassword(e) {
+    if (e.target.value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/)) {
+      setPwdMsg("Password is strong");
+    } else {
+      setPwdMsg("Password is weak");
+    }
+  }
+
   return (
     <div className="Container-fluid">
       <h2>Register User</h2>
       <dl>
         <dt>User Id</dt>
         <dd>
-          <input type="text" onKeyUp={VerifyUserId}></input>
+          <input
+            type="text"
+            onKeyUp={VerifyUserId}
+            onBlur={HideUserMsg}
+          ></input>
         </dd>
-        <dd className={isUserValid == true ? "text-success" : "text-danger"}>
+        <dd className={isUserValid ? "text-success" : "text-danger"}>
           {useMsg}
+        </dd>
+        <dt>Password</dt>
+        <dd>
+          <input type="password" onKeyUp={VerifyPassword}></input>
+        </dd>
+        <dd
+          className={
+            pwdMsg === "Password is strong" ? "text-success" : "text-danger"
+          }
+        >
+          {pwdMsg}
         </dd>
       </dl>
     </div>
   );
 }
+
+export default FormComponent;
